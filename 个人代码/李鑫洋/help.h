f0349b7d72063c3ac8d3eb670cc6d12c31396073
鑫洋 李 <lxyphoenix@outlook.com>
2019-11-04 16:48:56 +0800

    update

---
+++ 个人代码/李鑫洋/help.h
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<windows.h>
+
+
+void welcomePage()
+	{
+		
+		system("color F5");
+		printf("        ********************************************************************************\n");
+		printf("        *                                                                              *\n");
+		printf("        *                                                                              *\n");
+		printf("        *   *     *     *   ******  *        *****   ****      *     *     ******      *\n");
+		printf("        *   *    * *    *   *       *       *       *    *    **     **    *           *\n");
+		printf("        *    *  *   *  *    ******  *       *       *    *   *  *   *  *   ******      *\n");
+		printf("        *     **     **     *       *       *       *    *  *    * *    *  *           *\n");
+		printf("        *      *     *      ******  ******   *****   ****   *     *     *  ******      *\n");
+		printf("        *                                                                              *\n");
+		printf("        *                                                                              *\n");
+		printf("        *                                                                              *\n");
+		printf("        *                                                                              *\n");
+		printf("        *  Help: This is a calculator and the instrucions are as followed.             *\n");
+		printf("        *  Expressions include:                                                        *\n");
+		printf("        *  1. add(+)°¢subtract(-)°¢multiply(*)°¢divide(/)°¢modulo(%) and involution(^). *\n");
+		printf("        *  2. brackets(()).                                                            *\n");
+		printf("        *  Enter expressions please.<End it with °Æ=°Ø>:                               *\n");	
+		printf("        *                                                                              *\n");
+		printf("        ********************************************************************************\n");
+		
+		Sleep(10000);// sleep
+		system("cls"); //«Â∆¡ 
+	}
